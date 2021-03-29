9cde9ce798d262039c739d8e9a5b14b30ebc031b
bobbyg211 <robertdg96@gmail.com>
2021-03-29 17:37:34 -0400

    Complete

--- src/Router.js
+++ src/Router.js
@@ -8,10 +8,25 @@ to the About component.
 Remember to export the component at the end
 */
 
-import React from 'react'
-import { Switch, Route } from 'react-router'
+import React from "react";
+import { Switch, Route } from "react-router";
+import Home from "./components/Home";
+import About from "./components/About";
+import Car from "./components/Car";
 // Write component imports here //
 
+function Router() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/car/:id" component={Car} />
+      </Switch>
+    </div>
+  );
+}
 
+export default Router;
 
 // Start Router function here //
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import cars from '../cars.json'
+import React from "react";
+import cars from "../cars.json";
+import { Card, CardContent, Typography, Chip } from "@material-ui/core";
 // import material ui components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
-}
+  const id = props.match.params.id;
+  const car = cars.find((c) => c.id == id);
+  console.log(id);
+  console.log(cars);
+  return (
+    <div className="car">
+      <Card>
+        <CardContent>
+          <Typography>
+            <h2>{cars[id].Name}</h2>
+          </Typography>
+          {Object.keys(car).map((key, idx) => {
+            return <Chip label={`${key}: ${car[key]}`}></Chip>;
+          })}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
-export default Car
+export default Car;
